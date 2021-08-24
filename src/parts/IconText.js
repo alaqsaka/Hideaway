ee857ffa36081c2d9a1b01f86a20d34cddff930a
alaqsaka <aqsaabidin15@gmail.com>
2021-08-24 23:19:28 +0700

    chore: creating brand-text-icon

---
+++ src/parts/IconText.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Button from "elements/button";
+
+export default function iconText() {
+  return (
+    <Button className="brand-text-icon" href="" type="link">
+      Hide<span className="text-gray-900">away.</span>
+    </Button>
+  );
+}
