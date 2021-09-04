bac370848e166be3853b8ed9aac39be30435f52e
alaqsaka <aqsaabidin15@gmail.com>
2021-09-04 23:59:12 +0700

    chore: creating markup for mostpicked section

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -4,6 +4,17 @@ export default function MostPicked(props) {
   return (
     <section className="container">
       <h4 className="mb-3">Most Picked</h4>
+      <div className="container-grid">
+        {props.data.map((item, index) => {
+          return (
+            <div
+              className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
+            >
+              <div className="card">Card {index + 1}</div>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 }
