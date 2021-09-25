daeddb960d476e09dc3bd40d4021f33dc098309b
alaqsaka <aqsaabidin15@gmail.com>
2021-09-26 00:42:51 +0700

    chore: adding footer styling

---
+++ src/parts/Footer.js
@@ -0,0 +1,87 @@
+import React from "react";
+import Button from "elements/button";
+import IconText from "./IconText";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col-auto" style={{ width: 350 }}>
+            <IconText />
+            <p className="brand-tagline">
+              Providing the best accomodation for your family needs.
+            </p>
+          </div>
+          <div className="col-auto mr-5">
+            <h6 className="mt-2">For Beginners</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button type="link" href="/register">
+                  New Account
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/properties">
+                  Start Booking a Room
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/use-payments">
+                  Use Payments
+                </Button>
+              </li>
+            </ul>
+          </div>
+          <div className="col-2 mr-5">
+            <h6 className="mt-2">Explore Us</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button type="link" href="/career">
+                  Our Carrers
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/privacy">
+                  Privacy
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/terms">
+                  Terms & Conditions
+                </Button>
+              </li>
+            </ul>
+          </div>
+          <div className="col-3">
+            <h6 className="mt-2">Connect Us</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button
+                  isExternal
+                  type="link"
+                  href="mailto:support@hideaway.travel"
+                >
+                  support@hideaway.travel
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button isExternal type="link" href="tel:+6219316746">
+                  021 - 931 - 6746
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <span>BSD, Tangerang Selatan, Indonesia</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col text-center copyrights">
+            Copyright 2021 • All rights reserved • Hideaway
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
